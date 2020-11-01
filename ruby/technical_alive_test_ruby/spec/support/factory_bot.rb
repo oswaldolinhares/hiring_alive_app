da311ffe569916e98e92cae6a7e3d2894c6949e5
Oswaldo Linhares <oswaldolinhares@gmail.com>
2020-11-01 11:55:24 -0300

    Add factory_bot_rails

---
+++ ruby/technical_alive_test_ruby/spec/support/factory_bot.rb
@@ -0,0 +1,11 @@
+FactoryBot::SyntaxRunner.class_eval do
+  include ActionDispatch::TestProcess
+end
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryBot.lint
+  end
+end
